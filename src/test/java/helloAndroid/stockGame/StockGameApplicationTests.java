45f64d0f4d78c7a29c14387aa8963cb9809314ce
주진성 <jjsair0412@naver.com>
2021-08-10 23:46:22 +0900

    Initial commit

---
+++ src/test/java/helloAndroid/stockGame/StockGameApplicationTests.java
@@ -0,0 +1,13 @@
+package helloAndroid.stockGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
