842ce7916e6069b5c4f1ba142f259d9652c86130
주진성 <jjsair0412@naver.com>
2021-08-18 01:05:34 +0900

    MainController TestCode작성중 -2

--- src/test/java/helloAndroid/stockGame/Controller/MainControllerTest.java
+++ src/test/java/helloAndroid/stockGame/Controller/MainControllerTest.java
@@ -1,15 +1,11 @@
 package helloAndroid.stockGame.Controller;
 
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
